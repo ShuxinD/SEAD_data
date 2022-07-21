1a3abe72518b0af52611fc2137d21168df146731
ShuxinD <shuxind11@gmail.com>
2022-07-21 12:38:06 -0400

    update downloadSEDAdata.R

--- downloadSEDAdata.R
+++ downloadSEDAdata.R
@@ -2,8 +2,11 @@
 #' created by Shuxin D
 
 dir_seda <- file.path(getwd(), "data", "downloaded_from_SEDA")  # Create file path
-# dir_seda <- "/media/qnap3/Shuxin/SEDA/downloaded_from_SEDA" # due to the nature of QNAPs I cannot store the data under homefolder
+dir_seda <- "/media/qnap3/Shuxin/SEDA/downloaded_from_SEDA" # due to the nature of QNAPs I cannot store the data under homefolder
 dir_seda
+
+getOption("timeout")
+options(timeout = 600)
 
 # shapefile - version 4.0
 download.file("https://stacks.stanford.edu/file/druid:db586ns4974/seda_shapefiles_2019_4.0.zip", paste0(dir_seda,"/seda_shapefiles_2019_4.0.zip"))
